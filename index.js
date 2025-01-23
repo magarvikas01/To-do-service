7a8fa68562fe1fe26bc70f05f17306b596ce1215
magarvikas01 <v.magar@smartshiphub.com>
2025-01-23 22:00:07 +0530

    refactor database connection by removing pg module and integrating @fastify/postgres; update routes to handle database queries

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import routes from './routes/index.js'
-
+import postgres from '@fastify/postgres'
 
 const fastify = Fastify({
   logger: true
 })
+
+// database connection 
+await fastify.register( postgres , {
+  connectionString: `postgres://postgres:postgres@localhost:5432/local`})
 
 await fastify.register(cors, {
   hook: 'preHandler',
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 // routes/root.js
 export default async function (fastify) {
-  fastify.get('/', async function handler (request, reply) {
-    return { hello: 'world' }
-  })
+    fastify.get('/', async (req, reply) => {
+        try {
+          const result = await fastify.pg.query('SELECT * FROM todo.todo ORDER BY id ASC');
+          reply.send(result.rows); 
+        } catch (err) {
+          fastify.log.error(err.message);
+          reply.status(500).send({ error: 'Server error' });
+        }
+      });
 }
